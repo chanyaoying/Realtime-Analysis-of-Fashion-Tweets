b31c94927c93a6d0685634542406978163c71b10
Yash <yash.gadodia@ninjavan.co>
2021-10-09 12:11:06 +0800

    WIP: Consume tweets

--- bin/processStream.py
+++ bin/processStream.py
@@ -1,4 +1,4 @@
-"""Consumes stream for printing all messages to the console."""
+"""Generates a twitter stream to Kafka"""
 
 import os
 import tweepy as tw
@@ -8,38 +8,37 @@ import json
 import sys
 import time
 import socket
-from confluent_kafka import Consumer, KafkaError, KafkaException
+from dotenv import load_dotenv
+from confluent_kafka import Producer, KafkaError, KafkaException
 from tweepy.streaming import StreamListener
 from tweepy import Stream
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 
+load_dotenv("/.env")
 
 #############################
 ## HELPER FUNCTIONS
 #############################
 
-def msg_process(msg):
-    # Print the current time and the message.
-    time_start = time.strftime("%Y-%m-%d %H:%M:%S")
-    val = msg.value()
-    dval = json.loads(val)
-    print(time_start, dval)
+def acked(err, msg):
+    if err is not None:
+        print("Failed to deliver message: %s: %s" % (str(msg.value()), str(err)))
+    else:
+        print("Message produced: %s" % (str(msg.value())))
 
+class TweetListener(StreamListener):
 
-output_file = 'Tweets_covid.txt'
-class MyListener(StreamListener):
-    def __init__(self):
-        # initialise the kafka consumer
-        print('yo')
+    def __init__(self, conf, topic):
+        # initialise the kafka producer 
+        self.producer = Producer(conf)
+        self.topic = topic
         
     def on_data(self, data):
         try:
-            # IMPORTANT: 'a' for appending to the existing file content
-            # write to kafka consumer 
-            with open(output_file, 'a') as f:
-                f.write(data)
-                return True
+            # write to kafka producer
+            result = json.dumps(data)
+            self.producer.produce(self.topic, key="key", value=result, callback=acked)
         except BaseException as e:
             print("Error on_data: %s" % str(e))
         return True
@@ -48,14 +47,20 @@ class MyListener(StreamListener):
         print(status)
         return True
 
-
-#############################
-## MAIN FUNCTION
-#############################
-
 def main():
 
-    #TODO: create env file for keys/tokens
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('topic', type=str,
+                        help='Name of the Kafka topic to stream.')
+    args = parser.parse_args()
+
+    topic = args.topic
+
+    conf = {'bootstrap.servers': "localhost:9092",
+            'client.id': socket.gethostname()}
+            
+    ###################################
+
     consumer_key= 'xsoYzI8TDAGyZQBIhaE6gY5ZI'
     consumer_secret= 'iOYYy64CazvdHiGZZgGpUhguowpPjVzGa59XGlCTF8MaA0xAoI'
     access_token= '2365205522-hkfTmRYn8qXxBnUqWFE6MV0XQPwr7rYqXDWcFLy'
@@ -65,54 +70,11 @@ def main():
     auth.set_access_token(access_token, access_token_secret)
 
     # Define the search term and the date_since date as variables
-    twitter_stream = Stream(auth, MyListener())
-    query_terms = ['covid', 'covid19', 'coronavirus', 'wuhanvirus']
+    twitter_stream = Stream(auth, TweetListener(conf, topic))
+    query_terms = ['covid', 'corona', '19']
     twitter_stream.filter(track=query_terms)
 
     #####################################################
-
-    parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('topic', type=str,
-                        help='Name of the Kafka topic to stream: ')
-
-    args = parser.parse_args()
-
-    conf = {'bootstrap.servers': 'localhost:9092',
-            'default.topic.config': {'auto.offset.reset': 'smallest'},
-            'group.id': socket.gethostname()}
-
-    consumer = Consumer(conf)
-
-    running = True
-
-    try:
-        while running:
-            consumer.subscribe([args.topic])
-
-            msg = consumer.poll(1)
-            if msg is None:
-                continue
-
-            if msg.error():
-                if msg.error().code() == KafkaError._PARTITION_EOF:
-                    # End of partition event
-                    sys.stderr.write('%% %s [%d] reached end at offset %d\n' %
-                                     (msg.topic(), msg.partition(), msg.offset()))
-                elif msg.error().code() == KafkaError.UNKNOWN_TOPIC_OR_PART:
-                    sys.stderr.write('Topic unknown, creating %s topic\n' %
-                                     (args.topic))
-                elif msg.error():
-                    raise KafkaException(msg.error())
-            else:
-                msg_process(msg)
-
-    except KeyboardInterrupt:
-        pass
-
-    finally:
-        # Close down consumer to commit final offsets.
-        consumer.close()
-
 
 if __name__ == "__main__":
     main()
