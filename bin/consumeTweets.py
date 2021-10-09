ead27e153b128c8d4d08f46b8f6ad271b9f7b19d
chanyaoying <me@chanyaoying.com>
2021-10-09 15:04:37 +0800

    WIP: Created stream source, cannot read

--- bin/consumeTweets.py
+++ bin/consumeTweets.py
@@ -8,9 +8,21 @@ spark = SparkSession \
 
 # Subscribe to 1 topic
 df = spark \
-  .readStream \
-  .format("kafka") \
-  .option("kafka.bootstrap.servers", "localhost:9092") \
-  .option("subscribe", "test_tweets") \
-  .load()
-df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)").collect()
+    .readStream \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", "localhost:9092") \
+    .option("subscribe", "test_tweets") \
+    .load()
+
+# for fun
+spark.sparkContext.setCheckpointDir('/is459-project/spark-checkpoint')
+
+#!! WARNING !! THIS RUNS FOREVER
+# supposedly creates a sink for the stream
+df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)") \
+    .writeStream \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", "localhost:9092") \
+    .option("checkpointLocation", "/is459-project/spark-checkpoint") \
+    .option("topic", "test_tweets") \
+    .start() 
