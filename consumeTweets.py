fcc68538bec200a96d01db23cd92bf5190d269cf
YangZhouYiFan <zyfyang.2018@smu.edu.sg>
2021-11-09 02:37:35 +0800

    push code for data cleaning

--- consumeTweets.py
+++ consumeTweets.py
@@ -7,9 +7,12 @@
 ####
 
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import window, col
 from pyspark.sql.types import StringType
 from pymongo import MongoClient
+from pyspark.sql.functions import udf, from_json, col, window
+from pyspark.sql.types import StringType, StructType, StructField, ArrayType
+from datetime import datetime
+import re
 
 
 #####################################
@@ -32,6 +35,22 @@ spark = SparkSession \
 client = MongoClient('localhost', 27017)
 db = client.realtime_tweets_analysis
 
+
+#####################################
+# HELPER FUNCTIONS
+#####################################
+
+# Changing datetime format
+date_process = udf( \
+    lambda x: datetime.strftime( \
+        datetime.strptime(x,'%a %b %d %H:%M:%S +0000 %Y'), '%Y-%m-%d %H:%M:%S'))
+
+# Text Cleaning
+pre_process = udf( \
+    lambda x: re.sub(r'[^A-Za-z\n ]|(http\S+)|(www.\S+)', '', \
+        x.lower().strip()).split(), ArrayType(StringType()))
+
+
 #####################################
 # FOR EACH BATCH FUNCTION
 #####################################
@@ -44,10 +63,17 @@ def insert_to_DB(batchDF, epochID):
     
 
 #####################################
-# GLOBAL CONSTANTS
+# PREPROCESSING
 #####################################
 
 # Subscribe to 1 kafka topic
+
+schema = StructType( \
+    [StructField("created_at", StringType()), \
+    StructField("text", StringType())] \
+    )
+
+
 df = spark \
     .readStream \
     .format("kafka") \
@@ -56,33 +82,47 @@ df = spark \
     .option("includeTimestamp", "true") \
     .option("startingOffsets", "earliest") \
     .load() \
-    .selectExpr("CAST(value AS STRING)", "timestamp")
+    .selectExpr("CAST(timestamp AS TIMESTAMP) as timestamp", "CAST(value AS STRING) as message") \
+    .withColumn("value", from_json("message", schema)) \
+    .select('timestamp', 'value.*') \
+    .withColumn("created_at", date_process("created_at")) \
+    .withColumn("cleaned_data", pre_process("text")) \
+    .dropna()
 
-# TODO: Parse tweets to get only the text
+# remove text from other languages
+
+# lemmatise
+
+
+# # remove stopwords
+# stopwords_cleaner = StopWordsCleaner()\
+#       .setInputCols("normalized")\
+#       .setOutputCol("cleanTokens")\
+#       .setCaseSensitive(False)
 
 
 #####################################
 # SENTIMENT SCORING
 #####################################
 """
-No batching needed.
-1. Pass the text through the sentiment scoring model
-2. For each batch, 
+1. Batch them.
+2. Pass the text through the sentiment scoring model
+                                   [ text | score | hastag1 ]
+3. [ text | score | hastags ] -->  [ text | score | hastag2 ]
+                                   [ text | score | hastag3 ]
+4. group by hastags, mean the scores
+5. get top 10 hastags per batch
+6. For each batch, 
     a. Convert the batch into a dictionary
     b. Save it in MongoDB, under the collection sentimentScoring
 """
 
-# windowedCounts = df.withWatermark("timestamp", "30 seconds") \
-#     .groupBy(
-#         window("timestamp", "30 seconds", "15 seconds"), 'value'
-#     ) \
-#     .count()
-
-# # pipes real-time stream into console (for testing)
-# query = windowedCounts.writeStream \
-#     .outputMode("append") \
-#     .format("console") \
-#     .start()
+#!! Write to console (for now)
+sentiment_scoring = df \
+    .writeStream \
+    .format("console") \
+    .outputMode("append") \
+    .start()
 
 
 #####################################
@@ -94,15 +134,15 @@ No batching needed.
 3. 
 """
 
-topic_modelling_output = df.writeStream \
-    .outputMode('append') \
-    .foreachBatch(insert_to_DB) \
-    .start()
+# topic_modelling_output = df.writeStream \
+#     .outputMode('append') \
+#     .foreachBatch(insert_to_DB) \
+#     .start()
 
 
-####################################
-# Await termination for both queries
-####################################
+# ####################################
+# # Await termination for both queries
+# ####################################
 
-# query.awaitTermination()
-topic_modelling_output.awaitTermination()
+sentiment_scoring.awaitTermination()
+# topic_modelling_output.awaitTermination()
