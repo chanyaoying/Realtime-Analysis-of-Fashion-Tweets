347e3e8b434e27fc04ecf80d86400179b5b059e7
YangZhouYiFan <zyfyang.2018@smu.edu.sg>
2021-11-10 21:43:25 +0800

    Update consumeTweets_v1.py

--- consumeTweets_v1.py
+++ consumeTweets_v1.py
@@ -17,6 +17,24 @@ from textblob import TextBlob
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
 from pyspark.sql import functions as F
+import re
+
+from sparknlp.base import *
+from sparknlp.annotator import *
+from sparknlp.pretrained import PretrainedPipeline
+import sparknlp
+from pyspark.ml import Pipeline
+from pyspark.ml.feature import CountVectorizer, IDF
+
+# importing some libraries
+import pandas as pd
+import pyspark
+
+# stuff we'll need for text processing
+from nltk.corpus import stopwords
+# stuff we'll need for building the model
+from pyspark.mllib.linalg import Vector, Vectors
+from pyspark.mllib.clustering import LDA, LDAModel
 
 
 
@@ -53,7 +71,18 @@ date_process = udf( \
 # Text Cleaning
 # pre_process = udf( \
 #     lambda x: re.sub(r'[^A-Za-z\n ]|(http\S+)|(www.\S+)|(@\w+)|(#)|(rt)|(:)', '', \
-#         x.lower().strip()).split(), ArrayType(StringType()))
+#         x.lower().strip()), ArrayType(StringType()))
+
+def line_preprocessing(lines):
+    lines = regexp_replace(lines, r'[^A-Za-z\n ]|(http\S+)|(www.\S+)', '')
+    lines = regexp_replace(lines, '@\w+', '')
+    lines = regexp_replace(lines, '#', '')
+    lines = regexp_replace(lines, 'RT', '')
+    lines = regexp_replace(lines, ':', '')
+    # lines = regexp_replace(lines, 'content', '')
+    # lines = regexp_replace(lines, 'author', '')
+
+    return lines
 
 
 def preprocessing(lines):
@@ -171,11 +200,106 @@ words_query = words \
 2. Add to topic modelling model
 3. 
 """
+### STEP 1: DATA PREPARATION --------------------------------------------------------------------------------
+
+# Spark NLP requires the input dataframe or column to be converted to document. 
+document_assembler = DocumentAssembler() \
+    .setInputCol("text_cleaned") \
+    .setOutputCol("document") \
+    .setCleanupMode("shrink")
+# Split sentence to tokens(array)
+tokenizer = Tokenizer() \
+  .setInputCols(["document"]) \
+  .setOutputCol("token")
+# clean unwanted characters and garbage
+normalizer = Normalizer() \
+    .setInputCols(["token"]) \
+    .setOutputCol("normalized")
+# remove stopwords
+stopwords_cleaner = StopWordsCleaner()\
+      .setInputCols("normalized")\
+      .setOutputCol("cleanTokens")\
+      .setCaseSensitive(False)
+# stem the words to bring them to the root form.
+stemmer = Stemmer() \
+    .setInputCols(["cleanTokens"]) \
+    .setOutputCol("stem")
+# Finisher is the most important annotator. Spark NLP adds its own structure when we convert each row in the dataframe to document. Finisher helps us to bring back the expected structure viz. array of tokens.
+finisher = Finisher() \
+    .setInputCols(["stem"]) \
+    .setOutputCols(["tokens"]) \
+    .setOutputAsArray(True) \
+    .setCleanAnnotations(False)
+
+
+# We build a ml pipeline so that each phase can be executed in sequence. This pipeline can also be used to test the model. 
+nlp_pipeline = Pipeline(
+    stages=[document_assembler, 
+            tokenizer,
+            normalizer,
+            stopwords_cleaner, 
+            stemmer, 
+            finisher])
+
+# data preprocessing of twitter text to remove symbols and special characters
+cleaned_df = df.select(line_preprocessing(col("text")).alias("text_cleaned"), "created_at")
+
+# train the pipeline
+nlp_model = nlp_pipeline.fit(cleaned_df)
+
+# apply the pipeline to transform dataframe.
+processed_df  = nlp_model.transform(cleaned_df)
+
+tokens_df = processed_df.select('created_at','tokens').limit(10000)
+
+
+### STEP 2: FEATURE ENGINEERING ----------------------------------------------------------------------------------------------
+cv = CountVectorizer(inputCol="tokens", outputCol="raw_features", vocabSize=5000, minDF=10.0)
+# train the model
+cv_model = cv.fit(tokens_df)
+# transform the data. Output column name will be features.
+vectorized_tokens = cv_model.transform(tokens_df)
+
+# # IDF
+# idf = IDF(inputCol="raw_features", outputCol="features")
+# idfModel = idf.fit(vectorized_tokens)
+# result_tfidf = idfModel.transform(vectorized_tokens) 
+
+### STEP 3: BUILD THE LDA MODEL -------------------------------------------------------------------------------------------------
+# num_topics = 3
+# lda = LDA(k=num_topics, maxIter=10)
+# model = lda.fit(vectorized_tokens)
+# ll = model.logLikelihood(vectorized_tokens)
+# lp = model.logPerplexity(vectorized_tokens)
+
+# num_topics = 10
+# max_iterations = 100
+# lda_model = LDA.train(result_tfidf[['index','features']].map(list), k=num_topics, maxIterations=max_iterations)
+
+### STEP 4: VISUALIZE TOPICS ------------------------------------------------------------------------------------
+# extract vocabulary from CountVectorizer
+# vocab = cv_model.vocabulary
+# topics = model.describeTopics()   
+# topics_rdd = topics.rdd
+# topics_words = topics_rdd\
+#        .map(lambda row: row['termIndices'])\
+#        .map(lambda idx_list: [vocab[idx] for idx in idx_list])\
+#        .collect()
+
+
+
 
 # topic_modelling_output = df.writeStream \
 #     .outputMode('append') \
 #     .foreachBatch(insert_to_DB) \
 #     .start()
+
+# for testing purposes
+topic_modelling_query = vectorized_tokens \
+    .writeStream \
+    .format("console") \
+    .outputMode("append") \
+    .start()
 
 
 # ####################################
@@ -183,4 +307,4 @@ words_query = words \
 # ####################################
 
 words_query.awaitTermination()
-# topic_modelling_output.awaitTermination()
+topic_modelling_query.awaitTermination()
